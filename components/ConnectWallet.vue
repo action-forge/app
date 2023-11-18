6ddb327a2be0ec37ffbf0e4a02ecb0300c895f53
Manthankumar Satani <satanimanthan@gmail.com>
2023-11-18 16:58:14 +0300

    fix: disconnect text

--- components/ConnectWallet.vue
+++ components/ConnectWallet.vue
@@ -5,11 +5,14 @@ const wallet = useWalletStore()
 
 <template>
   <button
-    class="flex items-center border border-green-light rounded-full p-2.75 px-5"
+    class="group flex items-center border border-green-light rounded-full p-2.75 px-5"
     @click="!wallet.isConnected ? wallet.connect() : wallet.disconnect()"
   >
-    <span class="font-semibold">
+    <span class="font-semibold" :class="[wallet.isConnected && 'group-hover:hidden']">
       {{ wallet.isConnected ? wallet.connectedAddressShort : 'Connect Wallet' }}
+    </span>
+    <span class="font-semibold hidden" :class="[wallet.isConnected && 'group-hover:inline']">
+      Disconnect
     </span>
   </button>
 </template>
